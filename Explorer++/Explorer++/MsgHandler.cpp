b02276408298cc422d92004f928ed3cfccefeee2
David Erceg <david@explorerplusplus.com>
2011-08-09 07:48:24 +0000

    Merging r338 from trunk.

--- Explorer++/Explorer++/MsgHandler.cpp
+++ Explorer++/Explorer++/MsgHandler.cpp
@@ -1882,6 +1882,7 @@ have their addresses locked will not change directory. */
 HRESULT Explorerplusplus::BrowseFolder(LPITEMIDLIST pidlDirectory,UINT wFlags)
 {
 	HRESULT hr = E_FAIL;
+	int iTabObjectIndex = -1;
 
 	if(!m_TabInfo[m_iObjectIndex].bAddressLocked)
 	{
@@ -1890,12 +1891,18 @@ HRESULT Explorerplusplus::BrowseFolder(LPITEMIDLIST pidlDirectory,UINT wFlags)
 		if(SUCCEEDED(hr))
 		{
 			PlayNavigationSound();
-			OnDirChanged(m_iObjectIndex);
 		}
+
+		iTabObjectIndex = m_iObjectIndex;
 	}
 	else
 	{
-		hr = CreateNewTab(pidlDirectory,NULL,NULL,TRUE,NULL);
+		hr = CreateNewTab(pidlDirectory,NULL,NULL,TRUE,&iTabObjectIndex);
+	}
+
+	if(SUCCEEDED(hr))
+	{
+		OnDirChanged(iTabObjectIndex);
 	}
 
 	return hr;
