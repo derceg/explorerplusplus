14ac23625cdd67b36e68948661c2b20b425ac263
David Erceg <david@explorerplusplus.com>
2014-01-21 07:29:33 +1100

    - Moved shell-related declarations to ShellHelper.h. - Moved function declaration in Helper.h.

--- Explorer++/Helper/Helper.h
+++ Explorer++/Helper/Helper.h
@@ -10,56 +10,6 @@ Windows. */
 #define WINDOWS_VISTA_SEVEN_MAJORVERSION	6
 #define WINDOWS_XP_MAJORVERSION				5
 
-/* The following declarations, relating to
-ICustomDestinationList, are only valid for
-Windows 7. If building for an earlier
-version of Windows, these declarations
-will be required.
-All three declarations are from
-ShObjIdl.h. */
-typedef /* [v1_enum] */ 
-enum KNOWNDESTCATEGORY
-	{	KDC_FREQUENT	= 1,
-	KDC_RECENT	= ( KDC_FREQUENT + 1 ) 
-	} 	KNOWNDESTCATEGORY;
-
-EXTERN_C const IID IID_ICustomDestinationList;
-
-MIDL_INTERFACE("6332debf-87b5-4670-90c0-5e57b408a49e")
-ICustomDestinationList : public IUnknown
-{
-public:
-	virtual HRESULT STDMETHODCALLTYPE SetAppID( 
-		/* [string][in] */ __RPC__in_string LPCWSTR pszAppID) = 0;
-
-	virtual HRESULT STDMETHODCALLTYPE BeginList( 
-		/* [out] */ __RPC__out UINT *pcMinSlots,
-		/* [in] */ __RPC__in REFIID riid,
-		/* [iid_is][out] */ __RPC__deref_out_opt void **ppv) = 0;
-
-	virtual HRESULT STDMETHODCALLTYPE AppendCategory( 
-		/* [string][in] */ __RPC__in_string LPCWSTR pszCategory,
-		/* [in] */ __RPC__in_opt IObjectArray *poa) = 0;
-
-	virtual HRESULT STDMETHODCALLTYPE AppendKnownCategory( 
-		/* [in] */ KNOWNDESTCATEGORY category) = 0;
-
-	virtual HRESULT STDMETHODCALLTYPE AddUserTasks( 
-		/* [in] */ __RPC__in_opt IObjectArray *poa) = 0;
-
-	virtual HRESULT STDMETHODCALLTYPE CommitList( void) = 0;
-
-	virtual HRESULT STDMETHODCALLTYPE GetRemovedDestinations( 
-		/* [in] */ __RPC__in REFIID riid,
-		/* [iid_is][out] */ __RPC__deref_out_opt void **ppv) = 0;
-
-	virtual HRESULT STDMETHODCALLTYPE DeleteList( 
-		/* [string][unique][in] */ __RPC__in_opt_string LPCWSTR pszAppID) = 0;
-
-	virtual HRESULT STDMETHODCALLTYPE AbortList( void) = 0;
-
-};
-
 /* SummaryInformation stream constants. */
 #define PROPERTY_ID_TITLE		0x02
 #define PROPERTY_ID_SUBJECT		0x03
@@ -92,6 +42,7 @@ DWORD			GetNumFileHardLinks(const TCHAR *lpszFileName);
 int				ReadFileProperty(const TCHAR *lpszFileName,DWORD dwPropertyId,TCHAR *lpszPropertyBuf,DWORD dwBufLen);
 BOOL			GetVersionInfoString(const TCHAR *szFileName,const TCHAR *szVersionInfo,TCHAR *szBuffer,UINT cbBufLen);
 BOOL			ReadImageProperty(const TCHAR *lpszImage,UINT PropertyId,void *pPropBuffer,DWORD dwBufLen);
+HRESULT			GetMediaMetadata(const TCHAR *szFileName, const TCHAR *szAttribute, BYTE **pszOutput);
 BOOL			IsImage(const TCHAR *FileName);
 WORD			GetFileLanguage(const TCHAR *szFullFileName);
 BOOL			GetFileProductVersion(const TCHAR *szFullFileName,DWORD *pdwProductVersionLS,DWORD *pdwProductVersionMS);
@@ -106,4 +57,3 @@ BOOL			GetFileNameFromUser(HWND hwnd,TCHAR *FullFileName,UINT cchMax,const TCHAR
 /* General helper functions. */
 HINSTANCE		StartCommandPrompt(const TCHAR *Directory, bool Elevated);
 void			GetCPUBrandString(char *pszCPUBrand, UINT cchBuf);
-HRESULT			GetMediaMetadata(const TCHAR *szFileName, const TCHAR *szAttribute, BYTE **pszOutput);
--- Explorer++/Helper/ShellHelper.h
+++ Explorer++/Helper/ShellHelper.h
@@ -17,6 +17,57 @@
 #define HIDA_GetPIDLFolder(pida) (LPCITEMIDLIST)(((LPBYTE)pida)+(pida)->aoffset[0])
 #define HIDA_GetPIDLItem(pida, i) (LPCITEMIDLIST)(((LPBYTE)pida)+(pida)->aoffset[i+1])
 
+/* The following declarations, relating to
+ICustomDestinationList, are only valid for
+Windows 7. If building for an earlier
+version of Windows, these declarations
+will be required.
+All three declarations are from
+ShObjIdl.h. */
+typedef /* [v1_enum] */
+enum KNOWNDESTCATEGORY
+{
+	KDC_FREQUENT = 1,
+	KDC_RECENT = (KDC_FREQUENT + 1)
+} 	KNOWNDESTCATEGORY;
+
+EXTERN_C const IID IID_ICustomDestinationList;
+
+MIDL_INTERFACE("6332debf-87b5-4670-90c0-5e57b408a49e")
+ICustomDestinationList : public IUnknown
+{
+public:
+	virtual HRESULT STDMETHODCALLTYPE SetAppID(
+		/* [string][in] */ __RPC__in_string LPCWSTR pszAppID) = 0;
+
+	virtual HRESULT STDMETHODCALLTYPE BeginList(
+		/* [out] */ __RPC__out UINT *pcMinSlots,
+		/* [in] */ __RPC__in REFIID riid,
+		/* [iid_is][out] */ __RPC__deref_out_opt void **ppv) = 0;
+
+	virtual HRESULT STDMETHODCALLTYPE AppendCategory(
+		/* [string][in] */ __RPC__in_string LPCWSTR pszCategory,
+		/* [in] */ __RPC__in_opt IObjectArray *poa) = 0;
+
+	virtual HRESULT STDMETHODCALLTYPE AppendKnownCategory(
+		/* [in] */ KNOWNDESTCATEGORY category) = 0;
+
+	virtual HRESULT STDMETHODCALLTYPE AddUserTasks(
+		/* [in] */ __RPC__in_opt IObjectArray *poa) = 0;
+
+	virtual HRESULT STDMETHODCALLTYPE CommitList(void) = 0;
+
+	virtual HRESULT STDMETHODCALLTYPE GetRemovedDestinations(
+		/* [in] */ __RPC__in REFIID riid,
+		/* [iid_is][out] */ __RPC__deref_out_opt void **ppv) = 0;
+
+	virtual HRESULT STDMETHODCALLTYPE DeleteList(
+		/* [string][unique][in] */ __RPC__in_opt_string LPCWSTR pszAppID) = 0;
+
+	virtual HRESULT STDMETHODCALLTYPE AbortList(void) = 0;
+
+};
+
 struct JumpListTaskInformation
 {
 	const TCHAR	*pszName;
