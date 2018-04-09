299c46c4d91bee16e7a91f5d56cdcec452dd8860
David Erceg <david@explorerplusplus.com>
2018-04-09 23:44:19 +1000

    Add explanatory comment to help file batch script

--- compile-help-file.bat
+++ compile-help-file.bat
@@ -1,3 +1,7 @@
+REM AppVeyor treats a non-zero return code as an error. This is an issue, as the
+REM hhc executable returns 1 on success and 0 on failure. This script only
+REM exists to invert the error code that's returned.
+
 hhc "%APPVEYOR_BUILD_FOLDER%\Documentation\User\Help Manual\explorer++.hhp"
 
 IF NOT errorlevel 1 (
