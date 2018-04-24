39f73b5227ee4e56a8067756f93e67a6330ed4a2
David Erceg <david@explorerplusplus.com>
2018-04-24 21:18:44 +1000

    Update theme to sphinx_rtd_theme

--- Documentation/User/Sphinx/conf.py
+++ Documentation/User/Sphinx/conf.py
@@ -76,13 +76,15 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'collapse_navigation': False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
