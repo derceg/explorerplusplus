cbbc50f0ba8649694a296062183d23a90fb1e16c
David Erceg <david@explorerplusplus.com>
2018-05-12 22:40:58 +1000

    Exclude global.rst from documentation build

--- Documentation/User/Sphinx/conf.py
+++ Documentation/User/Sphinx/conf.py
@@ -65,7 +65,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'global.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
