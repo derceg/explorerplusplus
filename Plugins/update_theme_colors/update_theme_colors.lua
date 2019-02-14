8877ce53a86d7acac5f561b43d971fe80b5e1a06
David Erceg <david@explorerplusplus.com>
2019-02-15 03:16:49 +1100

    Add comment to plugin script file

--- Plugins/update_theme_colors/update_theme_colors.lua
+++ Plugins/update_theme_colors/update_theme_colors.lua
@@ -1,2 +1,4 @@
+-- The first argument to these functions represents the background
+-- color, the second the text color.
 ui.setListViewColors("#1E1E1E", "#FFFFFF")
 ui.setTreeViewColors("#1E1E1E", "#FFFFFF")
