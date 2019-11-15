f4b80d03db82195eb4732b2cc5dee2a116e83cd1
David Erceg <david@explorerplusplus.com>
2019-11-15 15:24:37 +1100

    Add 32x32 and 48x48 icons
    This means that the 16x16 icons should display cleanly at the following
    DPI scaling factors:
    
    - 100%
    - 150%
    - 200%
    - 300%
    
    At other scaling factors, the icons will be shrunk/stretched as
    appropriate.
    
    Additionally, the 24x24 icons (currently only used on the main toolbar)
    should display cleanly at the following DPI scaling factors:
    
    - 100%
    - 133%
    - 200%

--- Explorer++/Explorer++/IconResourceLoader.cpp
+++ Explorer++/Explorer++/IconResourceLoader.cpp
@@ -17,259 +17,333 @@ const std::unordered_map<Icon, std::map<int, UINT>> ICON_RESOURCE_MAPPINGS = {
 		Icon::AddBookmark,
 		{
 			{16, IDB_ADD_BOOKMARK_16},
-			{24, IDB_ADD_BOOKMARK_24}
+			{24, IDB_ADD_BOOKMARK_24},
+			{32, IDB_ADD_BOOKMARK_32},
+			{48, IDB_ADD_BOOKMARK_48}
 		}
 	},
 	{
 		Icon::ArrowRight,
 		{
 			{16, IDB_ARROW_RIGHT_16},
-			{24, IDB_ARROW_RIGHT_24}
+			{24, IDB_ARROW_RIGHT_24},
+			{32, IDB_ARROW_RIGHT_32},
+			{48, IDB_ARROW_RIGHT_48}
 		}
 	},
 	{
 		Icon::Back,
 		{
 			{16, IDB_BACK_16},
-			{24, IDB_BACK_24}
+			{24, IDB_BACK_24},
+			{32, IDB_BACK_32},
+			{48, IDB_BACK_48}
 		}
 	},
 	{
 		Icon::Bookmarks,
 		{
 			{16, IDB_BOOKMARKS_16},
-			{24, IDB_BOOKMARKS_24}
+			{24, IDB_BOOKMARKS_24},
+			{32, IDB_BOOKMARKS_32},
+			{48, IDB_BOOKMARKS_48}
 		}
 	},
 	{
 		Icon::CloseButton,
 		{
 			{16, IDB_CLOSE_BUTTON_16},
-			{24, IDB_CLOSE_BUTTON_24}
+			{24, IDB_CLOSE_BUTTON_24},
+			{32, IDB_CLOSE_BUTTON_32},
+			{48, IDB_CLOSE_BUTTON_48}
 		}
 	},
 	{
 		Icon::CloseTab,
 		{
 			{16, IDB_CLOSE_TAB_16},
-			{24, IDB_CLOSE_TAB_24}
+			{24, IDB_CLOSE_TAB_24},
+			{32, IDB_CLOSE_TAB_32},
+			{48, IDB_CLOSE_TAB_48}
 		}
 	},
 	{
 		Icon::CommandLine,
 		{
 			{16, IDB_COMMAND_LINE_16},
-			{24, IDB_COMMAND_LINE_24}
+			{24, IDB_COMMAND_LINE_24},
+			{32, IDB_COMMAND_LINE_32},
+			{48, IDB_COMMAND_LINE_48}
 		}
 	},
 	{
 		Icon::CommandLineAdmin,
 		{
 			{16, IDB_COMMAND_LINE_ADMIN_16},
-			{24, IDB_COMMAND_LINE_ADMIN_24}
+			{24, IDB_COMMAND_LINE_ADMIN_24},
+			{32, IDB_COMMAND_LINE_ADMIN_32},
+			{48, IDB_COMMAND_LINE_ADMIN_48}
 		}
 	},
 	{
 		Icon::Copy,
 		{
 			{16, IDB_COPY_16},
-			{24, IDB_COPY_24}
+			{24, IDB_COPY_24},
+			{32, IDB_COPY_32},
+			{48, IDB_COPY_48}
 		}
 	},
 	{
 		Icon::CopyTo,
 		{
 			{16, IDB_COPY_TO_16},
-			{24, IDB_COPY_TO_24}
+			{24, IDB_COPY_TO_24},
+			{32, IDB_COPY_TO_32},
+			{48, IDB_COPY_TO_48}
 		}
 	},
 	{
 		Icon::CustomizeColors,
 		{
 			{16, IDB_CUSTOMIZE_COLORS_16},
-			{24, IDB_CUSTOMIZE_COLORS_24}
+			{24, IDB_CUSTOMIZE_COLORS_24},
+			{32, IDB_CUSTOMIZE_COLORS_32},
+			{48, IDB_CUSTOMIZE_COLORS_48}
 		}
 	},
 	{
 		Icon::Cut,
 		{
 			{16, IDB_CUT_16},
-			{24, IDB_CUT_24}
+			{24, IDB_CUT_24},
+			{32, IDB_CUT_32},
+			{48, IDB_CUT_48}
 		}
 	},
 	{
 		Icon::Delete,
 		{
 			{16, IDB_DELETE_16},
-			{24, IDB_DELETE_24}
+			{24, IDB_DELETE_24},
+			{32, IDB_DELETE_32},
+			{48, IDB_DELETE_48}
 		}
 	},
 	{
 		Icon::DeletePermanently,
 		{
 			{16, IDB_DELETE_PERMANENTLY_16},
-			{24, IDB_DELETE_PERMANENTLY_24}
+			{24, IDB_DELETE_PERMANENTLY_24},
+			{32, IDB_DELETE_PERMANENTLY_32},
+			{48, IDB_DELETE_PERMANENTLY_48}
 		}
 	},
 	{
 		Icon::Filter,
 		{
 			{16, IDB_FILTER_16},
-			{24, IDB_FILTER_24}
+			{24, IDB_FILTER_24},
+			{32, IDB_FILTER_32},
+			{48, IDB_FILTER_48}
 		}
 	},
 	{
 		Icon::Folder,
 		{
 			{16, IDB_FOLDER_16},
-			{24, IDB_FOLDER_24}
+			{24, IDB_FOLDER_24},
+			{32, IDB_FOLDER_32},
+			{48, IDB_FOLDER_48}
 		}
 	},
 	{
 		Icon::FolderTree,
 		{
 			{16, IDB_FOLDER_TREE_16},
-			{24, IDB_FOLDER_TREE_24}
+			{24, IDB_FOLDER_TREE_24},
+			{32, IDB_FOLDER_TREE_32},
+			{48, IDB_FOLDER_TREE_48}
 		}
 	},
 	{
 		Icon::Forward,
 		{
 			{16, IDB_FORWARD_16},
-			{24, IDB_FORWARD_24}
+			{24, IDB_FORWARD_24},
+			{32, IDB_FORWARD_32},
+			{48, IDB_FORWARD_48}
 		}
 	},
 	{
 		Icon::Help,
 		{
 			{16, IDB_HELP_16},
-			{24, IDB_HELP_24}
+			{24, IDB_HELP_24},
+			{32, IDB_HELP_32},
+			{48, IDB_HELP_48}
 		}
 	},
 	{
 		Icon::Lock,
 		{
 			{16, IDB_LOCK_16},
-			{24, IDB_LOCK_24}
+			{24, IDB_LOCK_24},
+			{32, IDB_LOCK_32},
+			{48, IDB_LOCK_48}
 		}
 	},
 	{
 		Icon::MassRename,
 		{
 			{16, IDB_MASS_RENAME_16},
-			{24, IDB_MASS_RENAME_24}
+			{24, IDB_MASS_RENAME_24},
+			{32, IDB_MASS_RENAME_32},
+			{48, IDB_MASS_RENAME_48}
 		}
 	},
 	{
 		Icon::MergeFiles,
 		{
 			{16, IDB_MERGE_FILES_16},
-			{24, IDB_MERGE_FILES_24}
+			{24, IDB_MERGE_FILES_24},
+			{32, IDB_MERGE_FILES_32},
+			{48, IDB_MERGE_FILES_48}
 		}
 	},
 	{
 		Icon::MoveTo,
 		{
 			{16, IDB_MOVE_TO_16},
-			{24, IDB_MOVE_TO_24}
+			{24, IDB_MOVE_TO_24},
+			{32, IDB_MOVE_TO_32},
+			{48, IDB_MOVE_TO_48}
 		}
 	},
 	{
 		Icon::NewFolder,
 		{
 			{16, IDB_NEW_FOLDER_16},
-			{24, IDB_NEW_FOLDER_24}
+			{24, IDB_NEW_FOLDER_24},
+			{32, IDB_NEW_FOLDER_32},
+			{48, IDB_NEW_FOLDER_48}
 		}
 	},
 	{
 		Icon::NewTab,
 		{
 			{16, IDB_NEW_TAB_16},
-			{24, IDB_NEW_TAB_24}
+			{24, IDB_NEW_TAB_24},
+			{32, IDB_NEW_TAB_32},
+			{48, IDB_NEW_TAB_48}
 		}
 	},
 	{
 		Icon::Options,
 		{
 			{16, IDB_OPTIONS_16},
-			{24, IDB_OPTIONS_24}
+			{24, IDB_OPTIONS_24},
+			{32, IDB_OPTIONS_32},
+			{48, IDB_OPTIONS_48}
 		}
 	},
 	{
 		Icon::Paste,
 		{
 			{16, IDB_PASTE_16},
-			{24, IDB_PASTE_24}
+			{24, IDB_PASTE_24},
+			{32, IDB_PASTE_32},
+			{48, IDB_PASTE_48}
 		}
 	},
 	{
 		Icon::PasteShortcut,
 		{
 			{16, IDB_PASTE_SHORTCUT_16},
-			{24, IDB_PASTE_SHORTCUT_24}
+			{24, IDB_PASTE_SHORTCUT_24},
+			{32, IDB_PASTE_SHORTCUT_32},
+			{48, IDB_PASTE_SHORTCUT_48}
 		}
 	},
 	{
 		Icon::Properties,
 		{
 			{16, IDB_PROPERTIES_16},
-			{24, IDB_PROPERTIES_24}
+			{24, IDB_PROPERTIES_24},
+			{32, IDB_PROPERTIES_32},
+			{48, IDB_PROPERTIES_48}
 		}
 	},
 	{
 		Icon::Refresh,
 		{
 			{16, IDB_REFRESH_16},
-			{24, IDB_REFRESH_24}
+			{24, IDB_REFRESH_24},
+			{32, IDB_REFRESH_32},
+			{48, IDB_REFRESH_48}
 		}
 	},
 	{
 		Icon::Rename,
 		{
 			{16, IDB_RENAME_16},
-			{24, IDB_RENAME_24}
+			{24, IDB_RENAME_24},
+			{32, IDB_RENAME_32},
+			{48, IDB_RENAME_48}
 		}
 	},
 	{
 		Icon::Search,
 		{
 			{16, IDB_SEARCH_16},
-			{24, IDB_SEARCH_24}
+			{24, IDB_SEARCH_24},
+			{32, IDB_SEARCH_32},
+			{48, IDB_SEARCH_48}
 		}
 	},
 	{
 		Icon::SelectColumns,
 		{
 			{16, IDB_SELECT_COLUMNS_16},
-			{24, IDB_SELECT_COLUMNS_24}
+			{24, IDB_SELECT_COLUMNS_24},
+			{32, IDB_SELECT_COLUMNS_32},
+			{48, IDB_SELECT_COLUMNS_48}
 		}
 	},
 	{
 		Icon::SplitFiles,
 		{
 			{16, IDB_SPLIT_FILES_16},
-			{24, IDB_SPLIT_FILES_24}
+			{24, IDB_SPLIT_FILES_24},
+			{32, IDB_SPLIT_FILES_32},
+			{48, IDB_SPLIT_FILES_48}
 		}
 	},
 	{
 		Icon::Undo,
 		{
 			{16, IDB_UNDO_16},
-			{24, IDB_UNDO_24}
+			{24, IDB_UNDO_24},
+			{32, IDB_UNDO_32},
+			{48, IDB_UNDO_48}
 		}
 	},
 	{
 		Icon::Up,
 		{
 			{16, IDB_UP_16},
-			{24, IDB_UP_24}
+			{24, IDB_UP_24},
+			{32, IDB_UP_32},
+			{48, IDB_UP_48}
 		}
 	},
 	{
 		Icon::Views,
 		{
 			{16, IDB_VIEWS_16},
-			{24, IDB_VIEWS_24}
+			{24, IDB_VIEWS_24},
+			{32, IDB_VIEWS_32},
+			{48, IDB_VIEWS_48}
 		}
 	}
 };
