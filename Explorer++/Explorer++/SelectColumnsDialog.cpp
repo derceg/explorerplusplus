71b793bed8f81ca0b4c5fad4434db0ab10dd844a
sean echevarria <gh@creepingfog.com>
2020-04-25 19:34:00 -0400

    undo

--- Explorer++/Explorer++/SelectColumnsDialog.cpp
+++ Explorer++/Explorer++/SelectColumnsDialog.cpp
@@ -48,16 +48,7 @@ INT_PTR SelectColumnsDialog::OnInitDialog()
 
 	for(const auto &column : currentColumns)
 	{
-		std::wstring text;
-		
-		try
-		{
-			text = ResourceHelper::LoadString(GetInstance(),ShellBrowser::LookupColumnNameStringIndex(column.id));
-		}
-		catch (const std::runtime_error& ex)
-		{
-			ex.what();
-		}
+		std::wstring text = ResourceHelper::LoadString(GetInstance(),ShellBrowser::LookupColumnNameStringIndex(column.id));
 
 		LVITEM lvItem;
 		lvItem.mask		= LVIF_TEXT|LVIF_PARAM;
