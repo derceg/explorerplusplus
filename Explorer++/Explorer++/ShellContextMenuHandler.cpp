5b6478a0ddac1fc1745708f8e2efd8c172b1be19
David Erceg <david@explorerplusplus.com>
2020-05-06 16:24:13 +1000

    Run clang-format on context menu support files

--- Explorer++/Explorer++/ShellContextMenuHandler.cpp
+++ Explorer++/Explorer++/ShellContextMenuHandler.cpp
@@ -16,7 +16,7 @@
 #include "../Helper/Macros.h"
 #include "../Helper/ShellHelper.h"
 
-#define MENU_OPEN_IN_NEW_TAB	(MAX_SHELL_MENU_ID + 1)
+#define MENU_OPEN_IN_NEW_TAB (MAX_SHELL_MENU_ID + 1)
 
 void Explorerplusplus::AddMenuEntries(PCIDLIST_ABSOLUTE pidlParent,
 	const std::vector<PITEMID_CHILD> &pidlItems, DWORD_PTR dwData, HMENU hMenu)
@@ -27,22 +27,22 @@ void Explorerplusplus::AddMenuEntries(PCIDLIST_ABSOLUTE pidlParent,
 
 	bool addNewTabMenuItem = false;
 
-	if(pfcmi->uFrom == FROM_LISTVIEW)
+	if (pfcmi->uFrom == FROM_LISTVIEW)
 	{
-		if(pidlItems.size() == 1)
+		if (pidlItems.size() == 1)
 		{
 			SFGAOF fileAttributes = SFGAO_FOLDER;
 
 			unique_pidl_absolute pidlComplete(ILCombine(pidlParent, pidlItems.front()));
 			GetItemAttributes(pidlComplete.get(), &fileAttributes);
 
-			if(fileAttributes & SFGAO_FOLDER)
+			if (fileAttributes & SFGAO_FOLDER)
 			{
 				addNewTabMenuItem = true;
 			}
 		}
 	}
-	else if(pfcmi->uFrom == FROM_TREEVIEW)
+	else if (pfcmi->uFrom == FROM_TREEVIEW)
 	{
 		/* The treeview only contains folders,
 		so the new tab menu item will always
@@ -50,16 +50,17 @@ void Explorerplusplus::AddMenuEntries(PCIDLIST_ABSOLUTE pidlParent,
 		addNewTabMenuItem = true;
 	}
 
-	if(addNewTabMenuItem)
+	if (addNewTabMenuItem)
 	{
-		std::wstring openInNewTabText = ResourceHelper::LoadString(m_hLanguageModule, IDS_GENERAL_OPEN_IN_NEW_TAB);
+		std::wstring openInNewTabText =
+			ResourceHelper::LoadString(m_hLanguageModule, IDS_GENERAL_OPEN_IN_NEW_TAB);
 
 		MENUITEMINFO mii;
-		mii.cbSize		= sizeof(mii);
-		mii.fMask		= MIIM_STRING|MIIM_ID;
-		mii.wID			= MENU_OPEN_IN_NEW_TAB;
-		mii.dwTypeData	= openInNewTabText.data();
-		InsertMenuItem(hMenu,1,TRUE,&mii);
+		mii.cbSize = sizeof(mii);
+		mii.fMask = MIIM_STRING | MIIM_ID;
+		mii.wID = MENU_OPEN_IN_NEW_TAB;
+		mii.dwTypeData = openInNewTabText.data();
+		InsertMenuItem(hMenu, 1, TRUE, &mii);
 	}
 }
 
@@ -68,18 +69,18 @@ BOOL Explorerplusplus::HandleShellMenuItem(PCIDLIST_ABSOLUTE pidlParent,
 {
 	auto *pfcmi = reinterpret_cast<FileContextMenuInfo_t *>(dwData);
 
-	if(StrCmpI(szCmd,_T("open")) == 0)
+	if (StrCmpI(szCmd, _T("open")) == 0)
 	{
 		/* If ppidl is NULL, open the item specified by pidlParent
 		in the current listview. If ppidl is not NULL, open each
 		of the items specified in ppidl. */
-		if(pidlItems.empty())
+		if (pidlItems.empty())
 		{
-			OpenItem(pidlParent,FALSE,FALSE);
+			OpenItem(pidlParent, FALSE, FALSE);
 		}
 		else
 		{
-			for(const auto &pidl : pidlItems)
+			for (const auto &pidl : pidlItems)
 			{
 				unique_pidl_absolute pidlComplete(ILCombine(pidlParent, pidl));
 				OpenItem(pidlComplete.get(), FALSE, FALSE);
@@ -90,39 +91,39 @@ BOOL Explorerplusplus::HandleShellMenuItem(PCIDLIST_ABSOLUTE pidlParent,
 
 		return TRUE;
 	}
-	else if(StrCmpI(szCmd,_T("rename")) == 0)
+	else if (StrCmpI(szCmd, _T("rename")) == 0)
 	{
-		if(pfcmi->uFrom == FROM_LISTVIEW)
+		if (pfcmi->uFrom == FROM_LISTVIEW)
 		{
 			OnFileRename();
 		}
-		else if(pfcmi->uFrom == FROM_TREEVIEW)
+		else if (pfcmi->uFrom == FROM_TREEVIEW)
 		{
 			m_shellTreeView->StartRenamingSelectedItem();
 		}
 
 		return TRUE;
 	}
-	else if(StrCmpI(szCmd,_T("copy")) == 0)
+	else if (StrCmpI(szCmd, _T("copy")) == 0)
 	{
-		if(pfcmi->uFrom == FROM_LISTVIEW)
+		if (pfcmi->uFrom == FROM_LISTVIEW)
 		{
 			OnListViewCopy(TRUE);
 		}
-		else if(pfcmi->uFrom == FROM_TREEVIEW)
+		else if (pfcmi->uFrom == FROM_TREEVIEW)
 		{
 			OnTreeViewCopy(TRUE);
 		}
 
 		return TRUE;
 	}
-	else if(StrCmpI(szCmd,_T("cut")) == 0)
+	else if (StrCmpI(szCmd, _T("cut")) == 0)
 	{
-		if(pfcmi->uFrom == FROM_LISTVIEW)
+		if (pfcmi->uFrom == FROM_LISTVIEW)
 		{
 			OnListViewCopy(FALSE);
 		}
-		else if(pfcmi->uFrom == FROM_TREEVIEW)
+		else if (pfcmi->uFrom == FROM_TREEVIEW)
 		{
 			OnTreeViewCopy(FALSE);
 		}
@@ -133,35 +134,36 @@ BOOL Explorerplusplus::HandleShellMenuItem(PCIDLIST_ABSOLUTE pidlParent,
 	return FALSE;
 }
 
-void Explorerplusplus::HandleCustomMenuItem(PCIDLIST_ABSOLUTE pidlParent,
-	const std::vector<PITEMID_CHILD> &pidlItems, int iCmd)
+void Explorerplusplus::HandleCustomMenuItem(
+	PCIDLIST_ABSOLUTE pidlParent, const std::vector<PITEMID_CHILD> &pidlItems, int iCmd)
 {
-	switch(iCmd)
+	switch (iCmd)
 	{
-		case MENU_OPEN_IN_NEW_TAB:
-			{
-				unique_pidl_absolute pidlComplete;
-				TCHAR szParsingPath[MAX_PATH];
-				BOOL bOpenInNewTab;
+	case MENU_OPEN_IN_NEW_TAB:
+	{
+		unique_pidl_absolute pidlComplete;
+		TCHAR szParsingPath[MAX_PATH];
+		BOOL bOpenInNewTab;
 
-				if(!pidlItems.empty())
-				{
-					pidlComplete.reset(ILCombine(pidlParent, pidlItems[0]));
+		if (!pidlItems.empty())
+		{
+			pidlComplete.reset(ILCombine(pidlParent, pidlItems[0]));
 
-					bOpenInNewTab = FALSE;
-				}
-				else
-				{
-					pidlComplete.reset(ILCloneFull(pidlParent));
+			bOpenInNewTab = FALSE;
+		}
+		else
+		{
+			pidlComplete.reset(ILCloneFull(pidlParent));
 
-					bOpenInNewTab = TRUE;
-				}
+			bOpenInNewTab = TRUE;
+		}
 
-				GetDisplayName(pidlComplete.get(),szParsingPath,SIZEOF_ARRAY(szParsingPath),SHGDN_FORPARSING);
-				m_tabContainer->CreateNewTab(szParsingPath, TabSettings(_selected = true));
+		GetDisplayName(
+			pidlComplete.get(), szParsingPath, SIZEOF_ARRAY(szParsingPath), SHGDN_FORPARSING);
+		m_tabContainer->CreateNewTab(szParsingPath, TabSettings(_selected = true));
 
-				m_bTreeViewOpenInNewTab = TRUE;
-			}
-			break;
+		m_bTreeViewOpenInNewTab = TRUE;
+	}
+	break;
 	}
 }
