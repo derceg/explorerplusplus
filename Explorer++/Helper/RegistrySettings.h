3e38d5388d7b8195743e8d2c1c2bf017ff5e697e
David Erceg <david@explorerplusplus.com>
2020-12-31 03:57:43 +1100

    Run clang-format on settings files

--- Explorer++/Helper/RegistrySettings.h
+++ Explorer++/Helper/RegistrySettings.h
@@ -5,20 +5,21 @@
 #pragma once
 
 #include <tchar.h>
-#include <string>
 #include <list>
+#include <string>
 
 namespace RegistrySettings
 {
-	LONG	SaveDword(HKEY hKey,const TCHAR *valueName,DWORD dwValue);
-	LONG	ReadDword(HKEY hKey,const TCHAR *valueName,DWORD *pReturnValue);
-	LONG	SaveString(HKEY hKey,const TCHAR *valueName,const TCHAR *szValue);
-	LONG	ReadString(HKEY hKey,const TCHAR *szKey,TCHAR *valueName,DWORD cchMax);
-	LONG	ReadString(HKEY hKey,const std::wstring &valueName,std::wstring &strOutput);
-	LONG	SaveStringList(HKEY hKey,const TCHAR *baseValueName,const std::list<std::wstring> &strList);
-	LONG	ReadStringList(HKEY hKey,const TCHAR *baseValueName,std::list<std::wstring> &strList);
-	bool	SaveDateTime(HKEY key, const std::wstring &baseValueName, const FILETIME &dateTime);
-	bool	ReadDateTime(HKEY key, const std::wstring &baseValueName, FILETIME &dateTime);
+	LONG SaveDword(HKEY hKey, const TCHAR *valueName, DWORD dwValue);
+	LONG ReadDword(HKEY hKey, const TCHAR *valueName, DWORD *pReturnValue);
+	LONG SaveString(HKEY hKey, const TCHAR *valueName, const TCHAR *szValue);
+	LONG ReadString(HKEY hKey, const TCHAR *szKey, TCHAR *valueName, DWORD cchMax);
+	LONG ReadString(HKEY hKey, const std::wstring &valueName, std::wstring &strOutput);
+	LONG SaveStringList(
+		HKEY hKey, const TCHAR *baseValueName, const std::list<std::wstring> &strList);
+	LONG ReadStringList(HKEY hKey, const TCHAR *baseValueName, std::list<std::wstring> &strList);
+	bool SaveDateTime(HKEY key, const std::wstring &baseValueName, const FILETIME &dateTime);
+	bool ReadDateTime(HKEY key, const std::wstring &baseValueName, FILETIME &dateTime);
 
 	template <typename T>
 	LONG Read32BitValueFromRegistry(HKEY key, const std::wstring &valueName, T &output)
