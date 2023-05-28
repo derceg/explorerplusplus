b7cbd90ed44b72fd1a26f3f70da664c2ec6855a6
David Erceg <david@explorerplusplus.com>
2023-05-28 22:17:34 +1000

    Move DarkModeButton::DrawButtonText() into ThemeManager

--- Explorer++/Explorer++/ThemeManager.cpp
+++ Explorer++/Explorer++/ThemeManager.cpp
@@ -4,9 +4,9 @@
 
 #include "stdafx.h"
 #include "ThemeManager.h"
-#include "DarkModeButton.h"
 #include "DarkModeHelper.h"
 #include "Explorer++_internal.h"
+#include "../Helper/Controls.h"
 #include "../Helper/DpiCompatibility.h"
 #include "../Helper/WindowHelper.h"
 #include <wil/resource.h>
@@ -540,7 +540,9 @@ LRESULT ThemeManager::OnButtonCustomDraw(NMCUSTOMDRAW *customDraw)
 	{
 		auto style = GetWindowLongPtr(customDraw->hdr.hwndFrom, GWL_STYLE);
 
-		DarkModeButton::ButtonType buttonType;
+		// The size of the interactive element of the control (i.e. the check box or radio button
+		// part).
+		SIZE elementSize;
 
 		// Although the documentation
 		// (https://learn.microsoft.com/en-au/windows/win32/controls/button-styles#constants)
@@ -551,18 +553,67 @@ LRESULT ThemeManager::OnButtonCustomDraw(NMCUSTOMDRAW *customDraw)
 		// is set isn't going to work.
 		if ((style & BS_TYPEMASK) == BS_AUTOCHECKBOX)
 		{
-			buttonType = DarkModeButton::ButtonType::Checkbox;
+			elementSize = GetCheckboxSize(customDraw->hdr.hwndFrom);
 		}
 		else if ((style & BS_TYPEMASK) == BS_AUTORADIOBUTTON)
 		{
-			buttonType = DarkModeButton::ButtonType::Radio;
+			elementSize = GetRadioButtonSize(customDraw->hdr.hwndFrom);
 		}
 		else
 		{
 			break;
 		}
 
-		DarkModeButton::DrawButtonText(customDraw, buttonType);
+		// Also applies to radio buttons.
+		constexpr int CHECKBOX_TEXT_SPACING_96DPI = 3;
+
+		UINT dpi = DpiCompatibility::GetInstance().GetDpiForWindow(customDraw->hdr.hwndFrom);
+
+		RECT textRect = customDraw->rc;
+		textRect.left +=
+			elementSize.cx + MulDiv(CHECKBOX_TEXT_SPACING_96DPI, dpi, USER_DEFAULT_SCREEN_DPI);
+
+		std::wstring text = GetWindowString(customDraw->hdr.hwndFrom);
+		assert(!text.empty());
+
+		COLORREF textColor;
+
+		if (IsWindowEnabled(customDraw->hdr.hwndFrom))
+		{
+			textColor = DarkModeHelper::TEXT_COLOR;
+		}
+		else
+		{
+			textColor = DarkModeHelper::TEXT_COLOR_DISABLED;
+		}
+
+		SetTextColor(customDraw->hdc, textColor);
+
+		UINT textFormat = DT_LEFT;
+
+		if (!WI_IsFlagSet(customDraw->uItemState, CDIS_SHOWKEYBOARDCUES))
+		{
+			WI_SetFlag(textFormat, DT_HIDEPREFIX);
+		}
+
+		RECT finalTextRect = textRect;
+		DrawText(customDraw->hdc, text.c_str(), static_cast<int>(text.size()), &finalTextRect,
+			textFormat | DT_CALCRECT);
+
+		if (GetRectHeight(&finalTextRect) < GetRectHeight(&textRect))
+		{
+			textRect.top += (GetRectHeight(&textRect) - GetRectHeight(&finalTextRect)) / 2;
+		}
+
+		DrawText(customDraw->hdc, text.c_str(), static_cast<int>(text.size()), &textRect,
+			textFormat);
+
+		if (WI_IsFlagSet(customDraw->uItemState, CDIS_FOCUS))
+		{
+			DrawFocusRect(customDraw->hdc, &textRect);
+		}
+
+		// TODO: May also need to handle CDIS_DISABLED and CDIS_GRAYED.
 	}
 		return CDRF_SKIPDEFAULT;
 	}
