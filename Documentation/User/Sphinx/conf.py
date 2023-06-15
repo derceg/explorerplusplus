03900d55512379470515417d2654885ca09b92b1
David Erceg <david@explorerplusplus.com>
2023-06-16 02:06:51 +1000

    Update copyright date in Sphinx config

--- Documentation/User/Sphinx/conf.py
+++ Documentation/User/Sphinx/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Explorer++'
-copyright = '2018, Explorer++'
+copyright = '2023, Explorer++'
 author = 'Allen Titley'
 
 # The short X.Y version
