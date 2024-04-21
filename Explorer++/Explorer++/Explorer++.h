derceg/explorerplusplus

commit 24828ab0fb54ee0cba9a9ee6d3de9244b579d438

Allow navigation to search-ms: URLs
A search-ms: URL represents a search results folder. It's possible to
navigate to those folders, provided the STR_PARSE_PREFER_FOLDER_BROWSING
option is set when calling SHParseDisplayName(). Therefore, that's now
done.

Note that passing the STR_PARSE_PREFER_FOLDER_BROWSING option to
SHParseDisplayName() also appears to fix the issue described in
https://explorerplusplus.com/forum/viewtopic.php?t=3185. That is, when
navigating to a folder like the "Downloads" folder, the treeview
selection wouldn't be set.

From some investigation, it appears that's because the pidl returned by
SHParseDisplayName() would refer to an item in the root desktop folder,
but not the "Downloads" item that normally appears.

For whatever reason, passing the STR_PARSE_PREFER_FOLDER_BROWSING option
causes the pidl that's returned to refer to the filesystem folder
specifically, which means that folder is what will be selected in the
treeview.

This also fixes an issue that arose if the "Open zip files in
Explorer++" setting were disabled. In that situation, no other file
container types (e.g. .7z, .cab) would be opened in Explorer++ either.
Now, disabling that option only stops .zip files from being opened in
Explorer++. That means that opening a saved search file (.search-ms)
works consistently.

Explorer++/Explorer++/MsgHandler.cpp
@@ -115,8 +115,7 @@ void Explorerplusplus::OpenItem(const std::wstring &itemPath,
 	OpenFolderDisposition openFolderDisposition)
 {
 	unique_pidl_absolute pidlItem;
-	HRESULT hr =
-		SHParseDisplayName(itemPath.c_str(), nullptr, wil::out_param(pidlItem), 0, nullptr);
+	HRESULT hr = ParseDisplayNameForNavigation(itemPath, pidlItem);
 
 	if (SUCCEEDED(hr))
 	{
@@ -127,18 +126,28 @@ void Explorerplusplus::OpenItem(const std::wstring &itemPath,
 void Explorerplusplus::OpenItem(PCIDLIST_ABSOLUTE pidlItem,
 	OpenFolderDisposition openFolderDisposition)
 {
-	SFGAOF uAttributes = SFGAO_FOLDER | SFGAO_STREAM | SFGAO_LINK;
-	HRESULT hr = GetItemAttributes(pidlItem, &uAttributes);
+	SFGAOF attributes = SFGAO_FOLDER | SFGAO_STREAM | SFGAO_LINK;
+	HRESULT hr = GetItemAttributes(pidlItem, &attributes);
 
 	if (FAILED(hr))
 	{
 		return;
 	}
 
-	if ((uAttributes & SFGAO_FOLDER) && (uAttributes & SFGAO_STREAM))
+	if (WI_AreAllFlagsSet(attributes, SFGAO_FOLDER | SFGAO_STREAM))
 	{
-		/* Zip file. */
-		if (m_config->handleZipFiles)
+		// This is container file. Examples of these files include:
+		//
+		// - .7z
+		// - .cab
+		// - .search-ms
+		// - .zip
+
+		std::wstring parsingPath;
+		hr = GetDisplayName(pidlItem, SHGDN_FORPARSING, parsingPath);
+		bool isZipFile = (SUCCEEDED(hr) && parsingPath.ends_with(L".zip"));
+
+		if ((isZipFile && m_config->handleZipFiles) || !isZipFile)
 		{
 			OpenFolderItem(pidlItem, openFolderDisposition);
 		}
@@ -147,73 +156,25 @@ void Explorerplusplus::OpenItem(PCIDLIST_ABSOLUTE pidlItem,
 			OpenFileItem(pidlItem, EMPTY_STRING);
 		}
 	}
-	else if (uAttributes & SFGAO_FOLDER)
+	else if (WI_IsFlagSet(attributes, SFGAO_FOLDER))
 	{
 		OpenFolderItem(pidlItem, openFolderDisposition);
 	}
-	else if (uAttributes & SFGAO_LINK)
+	else if (WI_IsFlagSet(attributes, SFGAO_LINK))
 	{
-		/* This item is a shortcut. */
-		TCHAR szTargetPath[MAX_PATH];
+		unique_pidl_absolute target;
+		hr = MaybeResolveLinkTarget(m_hContainer, pidlItem, target);
 
-		std::wstring itemPath;
-		GetDisplayName(pidlItem, SHGDN_FORPARSING, itemPath);
-
-		hr = NFileOperations::ResolveLink(m_hContainer, 0, itemPath.c_str(), szTargetPath,
-			SIZEOF_ARRAY(szTargetPath));
-
-		if (hr == S_OK)
+		// If the target doesn't exist, MaybeResolveLinkTarget() will show an error message to the
+		// user. So, that case doesn't need to be handled at all here.
+		if (SUCCEEDED(hr))
 		{
-			/* The target of the shortcut was found
-			successfully. Query it to determine whether
-			it is a folder or not. */
-			uAttributes = SFGAO_FOLDER | SFGAO_STREAM;
-			hr = GetItemAttributes(szTargetPath, &uAttributes);
-
-			/* Note this is functionally equivalent to
-			recursively calling this function again.
-			However, the link may be arbitrarily deep
-			(or point to itself). Therefore, DO NOT
-			call this function recursively with itself
-			without some way of stopping. */
-			if (SUCCEEDED(hr))
-			{
-				/* Is this a link to a folder or zip file? */
-				if (((uAttributes & SFGAO_FOLDER) && !(uAttributes & SFGAO_STREAM))
-					|| ((uAttributes & SFGAO_FOLDER) && (uAttributes & SFGAO_STREAM)
-						&& m_config->handleZipFiles))
-				{
-					unique_pidl_absolute pidlTarget;
-					hr = SHParseDisplayName(szTargetPath, nullptr, wil::out_param(pidlTarget), 0,
-						nullptr);
-
-					if (SUCCEEDED(hr))
-					{
-						OpenFolderItem(pidlTarget.get(), openFolderDisposition);
-					}
-				}
-				else
-				{
-					hr = E_FAIL;
-				}
-			}
-		}
-
-		if (FAILED(hr))
-		{
-			/* It is possible the target may not resolve,
-			yet the shortcut is still valid. This is the
-			case with shortcut URL's for example.
-			Also, even if the shortcut points to a dead
-			folder, it should still attempted to be
-			opened. */
-			OpenFileItem(pidlItem, EMPTY_STRING);
+			OpenItem(target.get(), openFolderDisposition);
 		}
 	}
 	else
 	{
-		/* File item. */
-		OpenFileItem(pidlItem, EMPTY_STRING);
+		OpenFileItem(pidlItem, L"");
 	}
 }
 

Explorer++/Explorer++/Plugins/TabsApi/TabsApi.cpp
@@ -108,8 +108,7 @@ int Plugins::TabsApi::create(sol::table createProperties)
 	extractTabPropertiesForCreation(createProperties, tabSettings);
 
 	unique_pidl_absolute pidlDirectory;
-	HRESULT hr =
-		SHParseDisplayName(location->c_str(), nullptr, wil::out_param(pidlDirectory), 0, nullptr);
+	HRESULT hr = ParseDisplayNameForNavigation(location->c_str(), pidlDirectory);
 
 	if (FAILED(hr))
 	{

Explorer++/Explorer++/SearchDialog.cpp
@@ -647,18 +647,9 @@ INT_PTR SearchDialog::OnNotify(NMHDR *pnmhdr)
 				if (bRet)
 				{
 					auto itr = m_SearchItemsMapInternal.find(static_cast<int>(lvItem.lParam));
+					CHECK(itr != m_SearchItemsMapInternal.end());
 
-					/* Item should always exist. */
-					assert(itr != m_SearchItemsMapInternal.end());
-
-					unique_pidl_absolute pidlFull;
-					HRESULT hr = SHParseDisplayName(itr->second.c_str(), nullptr,
-						wil::out_param(pidlFull), 0, nullptr);
-
-					if (hr == S_OK)
-					{
-						m_browserWindow->OpenItem(pidlFull.get());
-					}
+					m_browserWindow->OpenItem(itr->second.c_str());
 				}
 			}
 		}

Explorer++/Explorer++/ShellBrowser/BrowsingHandler.cpp
@@ -126,11 +126,23 @@ void ShellBrowser::StoreCurrentlySelectedItems()
 HRESULT ShellBrowser::PerformEnumeration(NavigateParams &navigateParams,
 	std::vector<ShellBrowser::ItemInfo_t> &items)
 {
-	auto targetPidl = MaybeGetTargetPidlForNavigation(navigateParams.pidl);
+	// Note that although standard shortcuts (.lnk files) are currently handled outside this class,
+	// symlinks and virtual link objects aren't, so they will be handled here.
+	// Navigating to the target pidl is important for folders like the quick access folder. Although
+	// navigating directly to a recent/pinned folder works as expected, directory monitoring doesn't
+	// work. Presumably, that's because directory change notifications are only generated for the
+	// original (target) directory. To have things work correctly, the navigation needs to proceed
+	// to the original folder instead.
+	// Note that this call simply retrieves the target item, but doesn't attempt to resolve it. That
+	// matches the behavior of Explorer. For example, if a symlink to a directory is created and the
+	// target directory is then removed, Explorer will try to navigate to the target, without
+	// attempting to resolve the link.
+	unique_pidl_absolute targetPidl;
+	HRESULT hr = MaybeGetLinkTarget(navigateParams.pidl.Raw(), targetPidl);
 
-	if (targetPidl)
+	if (SUCCEEDED(hr))
 	{
-		navigateParams.pidl = *targetPidl;
+		navigateParams.pidl = targetPidl.get();
 	}
 
 	wil::com_ptr_nothrow<IShellFolder> parent;
@@ -162,44 +174,6 @@ HRESULT ShellBrowser::PerformEnumeration(NavigateParams &navigateParams,
 	m_navigationCommittedSignal(navigateParams);
 
 	return S_OK;
-}
-
-// If the specified item supports the IShellLink interface - that is, the item is a shortcut,
-// symlink or virtual link object, this function will return the target pidl. Note that, currently,
-// standard shortcuts aren't handled in the navigation code here, so this function is only used to
-// support the latter two cases.
-// Navigating to the target pidl is important for folders like the quick access folder. Although
-// navigating directly to a recent/pinned folder works as expected, directory monitoring doesn't
-// work. Presumably, that's because directory change notifications are only generated for the
-// original (target) directory. To have things work correctly, the navigation needs to proceed to
-// the original folder instead.
-std::optional<PidlAbsolute> ShellBrowser::MaybeGetTargetPidlForNavigation(const PidlAbsolute &pidl)
-{
-	wil::com_ptr_nothrow<IShellItem> shellItem;
-	HRESULT hr = SHCreateItemFromIDList(pidl.Raw(), IID_PPV_ARGS(&shellItem));
-
-	if (FAILED(hr))
-	{
-		return std::nullopt;
-	}
-
-	wil::com_ptr_nothrow<IShellLink> shellLink;
-	hr = shellItem->BindToHandler(nullptr, BHID_SFUIObject, IID_PPV_ARGS(&shellLink));
-
-	if (FAILED(hr))
-	{
-		return std::nullopt;
-	}
-
-	PidlAbsolute targetPidl;
-	hr = shellLink->GetIDList(PidlOutParam(targetPidl));
-
-	if (FAILED(hr))
-	{
-		return std::nullopt;
-	}
-
-	return targetPidl;
 }
 
 HRESULT ShellBrowser::EnumerateFolder(PCIDLIST_ABSOLUTE pidlDirectory, HWND owner, bool showHidden,

Explorer++/Explorer++/ShellBrowser/ShellBrowser.h
@@ -397,7 +397,6 @@ private:
 
 	/* Browsing support. */
 	HRESULT PerformEnumeration(NavigateParams &navigateParams, std::vector<ItemInfo_t> &items);
-	std::optional<PidlAbsolute> MaybeGetTargetPidlForNavigation(const PidlAbsolute &pidl);
 	static HRESULT EnumerateFolder(PCIDLIST_ABSOLUTE pidlDirectory, HWND owner, bool showHidden,
 		std::vector<ItemInfo_t> &items);
 	static std::optional<ItemInfo_t> GetItemInformation(IShellFolder *shellFolder,

Explorer++/Explorer++/ShellBrowser/ShellNavigationController.cpp
@@ -211,8 +211,7 @@ HRESULT ShellNavigationController::Navigate(const HistoryEntry *entry)
 HRESULT ShellNavigationController::Navigate(const std::wstring &path)
 {
 	unique_pidl_absolute pidlDirectory;
-	RETURN_IF_FAILED(
-		SHParseDisplayName(path.c_str(), nullptr, wil::out_param(pidlDirectory), 0, nullptr));
+	RETURN_IF_FAILED(ParseDisplayNameForNavigation(path.c_str(), pidlDirectory));
 
 	auto navigateParams = NavigateParams::Normal(pidlDirectory.get());
 	return Navigate(navigateParams);

Explorer++/Explorer++/TabContainer.cpp
@@ -866,17 +866,15 @@ Tab &TabContainer::CreateNewTab(const std::wstring &directory, const TabSettings
 	const FolderSettings *folderSettings, const FolderColumns *initialColumns)
 {
 	unique_pidl_absolute pidl;
-	HRESULT hr = SHParseDisplayName(directory.c_str(), nullptr, wil::out_param(pidl), 0, nullptr);
+	HRESULT hr = ParseDisplayNameForNavigation(directory.c_str(), pidl);
 
 	if (FAILED(hr))
 	{
-		hr = SHParseDisplayName(m_config->defaultTabDirectory.c_str(), nullptr,
-			wil::out_param(pidl), 0, nullptr);
+		hr = ParseDisplayNameForNavigation(m_config->defaultTabDirectory.c_str(), pidl);
 
 		if (FAILED(hr))
 		{
-			hr = SHParseDisplayName(m_config->defaultTabDirectoryStatic.c_str(), nullptr,
-				wil::out_param(pidl), 0, nullptr);
+			hr = ParseDisplayNameForNavigation(m_config->defaultTabDirectoryStatic.c_str(), pidl);
 		}
 	}
 

Explorer++/Explorer++/TabHandler.cpp
@@ -204,8 +204,7 @@ void Explorerplusplus::CreateCommandLineTabs()
 		}
 
 		unique_pidl_absolute fullPidl;
-		HRESULT hr = SHParseDisplayName(absolutePath->c_str(), nullptr, wil::out_param(fullPidl), 0,
-			nullptr);
+		HRESULT hr = ParseDisplayNameForNavigation(absolutePath->c_str(), fullPidl);
 
 		if (FAILED(hr))
 		{

commit 669e7b5c1b848dc2eb2717302ade2ee744315ac9

Fix shortcut handling regression
In 24828ab0fb54ee0cba9a9ee6d3de9244b579d438,
Explorerplusplus::OpenItem() was updated, with the code that handles
shortcut items being simplified.

Before that change, the type of a shortcut target (folder/non-folder)
was determined and the shortcut was opened via the shell if it was a
non-folder.

After the change, the shortcut target was executed directly. That isn't
correct however, since a shortcut can have various start options defined
(e.g. parameters, initial directory). Those options won't be applied if
the target is simply executed.

Therefore, Explorerplusplus::OpenItem() has been updated so that it
will open shortcuts to folder items directly, while passing shortcuts to
files to the shell. This matches the previous correct behaviour.

This issue was reported in
https://explorerplusplus.com/forum/viewtopic.php?t=3373.

Explorer++/Explorer++/Explorer++.h
@@ -449,6 +449,9 @@ private:
 		OpenFolderDisposition openFolderDisposition) override;
 	void OpenItem(PCIDLIST_ABSOLUTE pidlItem, OpenFolderDisposition openFolderDisposition) override;
 
+	void OpenShortcutItem(PCIDLIST_ABSOLUTE pidlItem, OpenFolderDisposition openFolderDisposition);
+	bool ShouldOpenContainerFile(PCIDLIST_ABSOLUTE pidlItem);
+
 	void OpenFolderItem(PCIDLIST_ABSOLUTE pidlItem,
 		OpenFolderDisposition openFolderDisposition = OpenFolderDisposition::CurrentTab);
 	void OpenFileItem(const std::wstring &itemPath, const std::wstring &parameters) override;

Explorer++/Explorer++/MsgHandler.cpp
@@ -143,11 +143,7 @@ void Explorerplusplus::OpenItem(PCIDLIST_ABSOLUTE pidlItem,
 		// - .search-ms
 		// - .zip
 
-		std::wstring parsingPath;
-		hr = GetDisplayName(pidlItem, SHGDN_FORPARSING, parsingPath);
-		bool isZipFile = (SUCCEEDED(hr) && parsingPath.ends_with(L".zip"));
-
-		if ((isZipFile && m_config->handleZipFiles) || !isZipFile)
+		if (ShouldOpenContainerFile(pidlItem))
 		{
 			OpenFolderItem(pidlItem, openFolderDisposition);
 		}
@@ -162,20 +158,78 @@ void Explorerplusplus::OpenItem(PCIDLIST_ABSOLUTE pidlItem,
 	}
 	else if (WI_IsFlagSet(attributes, SFGAO_LINK))
 	{
-		unique_pidl_absolute target;
-		hr = MaybeResolveLinkTarget(m_hContainer, pidlItem, target);
-
-		// If the target doesn't exist, MaybeResolveLinkTarget() will show an error message to the
-		// user. So, that case doesn't need to be handled at all here.
-		if (SUCCEEDED(hr))
-		{
-			OpenItem(target.get(), openFolderDisposition);
-		}
+		OpenShortcutItem(pidlItem, openFolderDisposition);
 	}
 	else
 	{
 		OpenFileItem(pidlItem, L"");
 	}
+}
+
+void Explorerplusplus::OpenShortcutItem(PCIDLIST_ABSOLUTE pidlItem,
+	OpenFolderDisposition openFolderDisposition)
+{
+	unique_pidl_absolute target;
+	HRESULT hr = MaybeResolveLinkTarget(m_hContainer, pidlItem, target);
+
+	if (FAILED(hr))
+	{
+		// If the target doesn't exist, MaybeResolveLinkTarget() will show an error message to the
+		// user. So, that case doesn't need to be handled at all here.
+		return;
+	}
+
+	bool openAsFolder = false;
+
+	SFGAOF targetAttributes = SFGAO_FOLDER | SFGAO_STREAM;
+	hr = GetItemAttributes(target.get(), &targetAttributes);
+
+	if (SUCCEEDED(hr))
+	{
+		bool isFolder = WI_IsFlagSet(targetAttributes, SFGAO_FOLDER)
+			&& WI_IsFlagClear(targetAttributes, SFGAO_STREAM);
+		bool isContainerFile = WI_IsFlagSet(targetAttributes, SFGAO_FOLDER)
+			&& WI_IsFlagSet(targetAttributes, SFGAO_STREAM);
+
+		openAsFolder = isFolder || (isContainerFile && ShouldOpenContainerFile(target.get()));
+	}
+
+	if (openAsFolder)
+	{
+		// This is a shortcut to a folder item or container file. In either case, it should be
+		// opened here, rather than being opened via the shell (since opening the shortcut via the
+		// shell will result in the item being opened in the default file manager).
+		OpenFolderItem(target.get(), openFolderDisposition);
+	}
+	else
+	{
+		// If the shortcut file points to something other than a folder/container file, the shortcut
+		// should be opened via the shell. It's important to do that, rather than executing the
+		// target directly, since the shortcut can have various start options defined (e.g.
+		// parameters, initial directory, window state). Those options won't be applied if the
+		// target is simply executed.
+		// This branch wil also be taken if the shortcut points to a .zip file and .zip file
+		// handling is turned off. In that situation, the shortcut should still be opened via the
+		// shell. That's because at least one of the shortcut options (window state) will be applied
+		// when opening the shortcut. That won't be the case if the target is executed directly.
+		OpenFileItem(pidlItem, L"");
+	}
+}
+
+// Returns true if the specified container file should be opened as a folder. If false, the file
+// should be opened via the shell.
+bool Explorerplusplus::ShouldOpenContainerFile(PCIDLIST_ABSOLUTE pidlItem)
+{
+	std::wstring parsingPath;
+	HRESULT hr = GetDisplayName(pidlItem, SHGDN_FORPARSING, parsingPath);
+
+	if (FAILED(hr))
+	{
+		return false;
+	}
+
+	bool isZipFile = (SUCCEEDED(hr) && parsingPath.ends_with(L".zip"));
+	return (isZipFile && m_config->handleZipFiles) || !isZipFile;
 }
 
 void Explorerplusplus::OpenFolderItem(PCIDLIST_ABSOLUTE pidlItem,