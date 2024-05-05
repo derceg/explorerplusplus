b476cbc4367fb55eecb4862818e52842f289cb77
David Erceg <david@explorerplusplus.com>
2024-05-05 15:04:25 +1000

    Remove outdated comment
    There is no need for a custom function that returns the size of a
    built-in array, since std::size() can be used.

--- Explorer++/Helper/Macros.h
+++ Explorer++/Helper/Macros.h
@@ -6,18 +6,6 @@
 
 #define EMPTY_STRING _T("")
 
-/* TODO: Switch to this once the compiler
-provides support for constexpr.
-See:
-http://www.reddit.com/r/programming/comments/i0gz4/chromiums_code_is_perhaps_the_most_quality_code/
-http://stackoverflow.com/questions/4748083/when-should-you-use-constexpr-capability-in-c0x
-*/
-// template <typename T,size_t N>
-// constexpr size_t SIZEOF_ARRAY(T (&)[N])
-//{
-//	return N;
-//}
-
 template <typename T, size_t N>
 char (&ArraySizeHelper(T (&array)[N]))[N];
 #define SIZEOF_ARRAY(array) (sizeof(ArraySizeHelper(array)))
