456575548d950698eb57b6d462bf5ce6c11ba9fd
David Erceg <david@explorerplusplus.com>
2025-01-28 14:29:04 +1100

    Add const to parameter in NavigationController::GetIndexOfEntry()

--- Explorer++/Explorer++/NavigationController.h
+++ Explorer++/Explorer++/NavigationController.h
@@ -60,7 +60,7 @@ public:
 		return m_entries[index].get();
 	}
 
-	std::optional<int> GetIndexOfEntry(HistoryEntryType *entry) const
+	std::optional<int> GetIndexOfEntry(const HistoryEntryType *entry) const
 	{
 		auto itr = std::find_if(m_entries.begin(), m_entries.end(),
 			[entry](auto &currentEntry) { return currentEntry.get() == entry; });
