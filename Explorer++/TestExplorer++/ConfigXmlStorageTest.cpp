d7db467cc99fda87ef9b1a4413c201a99a6a62b5
John Burnett <github@johnburnett.com>
2025-04-27 16:25:26 -0700

    Moving tests

--- Explorer++/TestExplorer++/ConfigXmlStorageTest.cpp
+++ Explorer++/TestExplorer++/ConfigXmlStorageTest.cpp
@@ -7,7 +7,6 @@
 #include "Config.h"
 #include "ConfigStorageTestHelper.h"
 #include "XmlStorageTestHelper.h"
-#include "../Explorer++/Storage.h"
 #include <gtest/gtest.h>
 
 class ConfigXmlStorageTest : public XmlStorageTest
@@ -25,24 +24,4 @@ TEST_F(ConfigXmlStorageTest, SaveLoad)
 	ConfigXmlStorage::Load(xmlDocumentData.rootNode.get(), loadedConfig);
 
 	EXPECT_EQ(loadedConfig, referenceConfig);
-}
-
-TEST_F(ConfigXmlStorageTest, ConfigEnvVar)
-{
-	auto set = SetEnvironmentVariable(Storage::CONFIG_FILE_ENV_VAR_NAME, NULL);
-	ASSERT_TRUE(set);
-	auto path = Storage::GetConfigFilePath();
-	ASSERT_TRUE(path.ends_with(L"config.xml"));
-
-	set = SetEnvironmentVariable(L"FOO", L"BAR");
-	ASSERT_TRUE(set);
-	set = SetEnvironmentVariable(Storage::CONFIG_FILE_ENV_VAR_NAME, L"%FOO%\\explorerpp.xml");
-	ASSERT_TRUE(set);
-	path = Storage::GetConfigFilePath();
-	ASSERT_EQ(path, L"BAR\\explorerpp.xml");
-
-	set = SetEnvironmentVariable(Storage::CONFIG_FILE_ENV_VAR_NAME, NULL);
-	ASSERT_TRUE(set);
-	path = Storage::GetConfigFilePath();
-	ASSERT_TRUE(path.ends_with(L"config.xml"));
 }
