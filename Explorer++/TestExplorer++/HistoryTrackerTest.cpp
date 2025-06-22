e628368e723157694a47b92964c0e06724acb4e4
David Erceg <david@explorerplusplus.com>
2025-06-22 17:07:12 +1000

    Update HistoryTrackerTest to use BrowserTestBase

--- Explorer++/TestExplorer++/HistoryTrackerTest.cpp
+++ Explorer++/TestExplorer++/HistoryTrackerTest.cpp
@@ -4,52 +4,22 @@
 
 #include "pch.h"
 #include "HistoryTracker.h"
-#include "BrowserWindowMock.h"
+#include "BrowserTestBase.h"
+#include "BrowserWindowFake.h"
 #include "HistoryModel.h"
-#include "ShellBrowser/NavigationEvents.h"
-#include "ShellBrowser/ShellNavigationController.h"
-#include "ShellBrowserFake.h"
-#include "ShellTestHelper.h"
-#include "TabEvents.h"
-#include "TabNavigationMock.h"
 #include <gtest/gtest.h>
 
 using namespace testing;
 
-class HistoryTrackerTest : public Test
+class HistoryTrackerTest : public BrowserTestBase
 {
 protected:
-	HistoryTrackerTest() :
-		m_historyTracker(&m_historyModel, &m_navigationEvents),
-		m_tab1(std::make_unique<ShellBrowserFake>(&m_navigationEvents, &m_tabNavigation1),
-			&m_browser1, nullptr, &m_tabEvents),
-		m_tab2(std::make_unique<ShellBrowserFake>(&m_navigationEvents, &m_tabNavigation1),
-			&m_browser1, nullptr, &m_tabEvents),
-		m_tab3(std::make_unique<ShellBrowserFake>(&m_navigationEvents, &m_tabNavigation2),
-			&m_browser2, nullptr, &m_tabEvents)
+	HistoryTrackerTest() : m_historyTracker(&m_historyModel, &m_navigationEvents)
 	{
 	}
-
-	void NavigateTab(Tab &tab, const PidlAbsolute &pidl)
-	{
-		auto navigateParams = NavigateParams::Normal(pidl.Raw());
-		tab.GetShellBrowser()->GetNavigationController()->Navigate(navigateParams);
-	}
-
-	TabEvents m_tabEvents;
-	NavigationEvents m_navigationEvents;
 
 	HistoryModel m_historyModel;
 	HistoryTracker m_historyTracker;
-
-	NiceMock<BrowserWindowMock> m_browser1;
-	NiceMock<TabNavigationMock> m_tabNavigation1;
-	Tab m_tab1;
-	Tab m_tab2;
-
-	NiceMock<BrowserWindowMock> m_browser2;
-	NiceMock<TabNavigationMock> m_tabNavigation2;
-	Tab m_tab3;
 };
 
 TEST_F(HistoryTrackerTest, CheckNavigationsAdded)
@@ -61,18 +31,22 @@ TEST_F(HistoryTrackerTest, CheckNavigationsAdded)
 	m_historyModel.AddHistoryChangedObserver(callback.AsStdFunction());
 	EXPECT_CALL(callback, Call()).Times(3);
 
-	auto pidlFake1 = CreateSimplePidlForTest(L"C:\\Fake1");
-	NavigateTab(m_tab1, pidlFake1);
+	auto *browser1 = AddBrowser();
+
+	PidlAbsolute pidl1;
+	browser1->AddTab(L"c:\\fake1", {}, &pidl1);
 	ASSERT_EQ(history.size(), 1U);
-	EXPECT_EQ(history[0], pidlFake1);
+	EXPECT_EQ(history[0], pidl1);
 
-	auto pidlFake2 = CreateSimplePidlForTest(L"C:\\Fake2");
-	NavigateTab(m_tab2, pidlFake2);
+	PidlAbsolute pidl2;
+	browser1->AddTab(L"c:\\path2", {}, &pidl2);
 	ASSERT_EQ(history.size(), 2U);
-	EXPECT_EQ(history[0], pidlFake2);
+	EXPECT_EQ(history[0], pidl2);
 
-	auto pidlFake3 = CreateSimplePidlForTest(L"C:\\Fake3");
-	NavigateTab(m_tab3, pidlFake3);
+	auto *browser2 = AddBrowser();
+
+	PidlAbsolute pidl3;
+	browser2->AddTab(L"c:\\path3", {}, &pidl3);
 	ASSERT_EQ(history.size(), 3U);
-	EXPECT_EQ(history[0], pidlFake3);
+	EXPECT_EQ(history[0], pidl3);
 }
