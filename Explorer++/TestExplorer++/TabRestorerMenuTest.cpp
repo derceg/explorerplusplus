6d333868ba76681784aaf61cd1a8e4609042c731
David Erceg <david@explorerplusplus.com>
2025-06-26 00:15:34 +1000

    Update TabRestorerMenuTest to use BrowserTestBase

--- Explorer++/TestExplorer++/TabRestorerMenuTest.cpp
+++ Explorer++/TestExplorer++/TabRestorerMenuTest.cpp
@@ -4,125 +4,100 @@
 
 #include "pch.h"
 #include "TabRestorerMenu.h"
-#include "AcceleratorManager.h"
-#include "BrowserList.h"
-#include "BrowserWindowMock.h"
+#include "BrowserTestBase.h"
+#include "BrowserWindowFake.h"
 #include "MenuViewFake.h"
 #include "MenuViewFakeTestHelper.h"
-#include "ResourceLoaderFake.h"
-#include "ShellBrowser/NavigationEvents.h"
-#include "ShellBrowser/ShellNavigationController.h"
-#include "ShellBrowserFake.h"
 #include "ShellIconLoaderFake.h"
-#include "ShellTestHelper.h"
-#include "TabEvents.h"
-#include "TabNavigationMock.h"
 #include "TabRestorer.h"
 #include <gtest/gtest.h>
 
 using namespace testing;
 
-class TabRestorerMenuTest : public Test
+class TabRestorerMenuTest : public BrowserTestBase
 {
 protected:
 	TabRestorerMenuTest() : m_tabRestorer(&m_tabEvents, &m_browserList)
 	{
 	}
 
-	Tab BuildTab()
-	{
-		return Tab(std::make_unique<ShellBrowserFake>(&m_navigationEvents, &m_tabNavigation),
-			&m_browser, nullptr, &m_tabEvents);
-	}
-
-	void NavigateTab(Tab &tab, const PidlAbsolute &pidl)
-	{
-		auto navigateParams = NavigateParams::Normal(pidl.Raw());
-		tab.GetShellBrowser()->GetNavigationController()->Navigate(navigateParams);
-	}
+	TabRestorer m_tabRestorer;
+	ShellIconLoaderFake m_shellIconLoader;
 
 	MenuViewFake m_menuView;
-	AcceleratorManager m_acceleratorManager;
-
-	TabEvents m_tabEvents;
-	BrowserList m_browserList;
-	TabRestorer m_tabRestorer;
-
-	ShellIconLoaderFake m_shellIconLoader;
-	ResourceLoaderFake m_resourceLoader;
-
-	NavigationEvents m_navigationEvents;
-	TabNavigationMock m_tabNavigation;
-	BrowserWindowMock m_browser;
 };
 
 TEST_F(TabRestorerMenuTest, CheckItems)
 {
-	auto tab1 = BuildTab();
-	auto path1 = CreateSimplePidlForTest(L"c:\\path1");
-	NavigateTab(tab1, path1);
-	m_tabEvents.NotifyPreRemoval(tab1, 0);
+	auto *browser = AddBrowser();
+	browser->AddTab(L"c:\\path1");
 
-	auto tab2 = BuildTab();
-	auto path2 = CreateSimplePidlForTest(L"c:\\path2");
-	NavigateTab(tab2, path2);
-	m_tabEvents.NotifyPreRemoval(tab2, 0);
+	PidlAbsolute pidl2;
+	int tabId2 = browser->AddTabAndReturnId(L"c:\\path2", {}, &pidl2);
+
+	PidlAbsolute pidl3;
+	int tabId3 = browser->AddTabAndReturnId(L"c:\\path3", {}, &pidl3);
+
+	auto *tabContainer = browser->GetActiveTabContainer();
+	EXPECT_TRUE(tabContainer->CloseTab(tabContainer->GetTab(tabId2)));
+	EXPECT_TRUE(tabContainer->CloseTab(tabContainer->GetTab(tabId3)));
 
 	TabRestorerMenu menu(&m_menuView, &m_acceleratorManager, &m_tabRestorer, &m_shellIconLoader,
 		&m_resourceLoader);
 
-	MenuViewFakeTestHelper::CheckItemDetails(&m_menuView, { path2, path1 });
+	MenuViewFakeTestHelper::CheckItemDetails(&m_menuView, { pidl3, pidl2 });
 }
 
 TEST_F(TabRestorerMenuTest, Selection)
 {
-	auto tab1 = BuildTab();
-	auto path1 = CreateSimplePidlForTest(L"c:\\path1");
-	NavigateTab(tab1, path1);
-	m_tabEvents.NotifyPreRemoval(tab1, 0);
+	auto *browser = AddBrowser();
+	browser->AddTab(L"c:\\path1");
+	int tabId2 = browser->AddTabAndReturnId(L"c:\\path2");
+	int tabId3 = browser->AddTabAndReturnId(L"c:\\path3");
+	int tabId4 = browser->AddTabAndReturnId(L"c:\\path4");
 
-	auto tab2 = BuildTab();
-	auto path2 = CreateSimplePidlForTest(L"c:\\path2");
-	NavigateTab(tab2, path2);
-	m_tabEvents.NotifyPreRemoval(tab2, 0);
-
-	auto tab3 = BuildTab();
-	auto path3 = CreateSimplePidlForTest(L"c:\\path3");
-	NavigateTab(tab3, path3);
-	m_tabEvents.NotifyPreRemoval(tab3, 0);
+	auto *tabContainer = browser->GetActiveTabContainer();
+	EXPECT_TRUE(tabContainer->CloseTab(tabContainer->GetTab(tabId2)));
+	EXPECT_TRUE(tabContainer->CloseTab(tabContainer->GetTab(tabId3)));
+	EXPECT_TRUE(tabContainer->CloseTab(tabContainer->GetTab(tabId4)));
 
 	TabRestorerMenu menu(&m_menuView, &m_acceleratorManager, &m_tabRestorer, &m_shellIconLoader,
 		&m_resourceLoader);
 
 	// Tabs are listed by most recently closed first, so the menu should contain:
 	//
+	// tab4
 	// tab3
 	// tab2
-	// tab1
 	//
-	// and this call should select tab2.
+	// and this call should select tab3.
 	m_menuView.SelectItem(m_menuView.GetItemId(1), false, false);
+	EXPECT_EQ(tabContainer->GetNumTabs(), 2);
 
-	// tab2 was restored, so tab1 and tab3 should remain.
-	ASSERT_EQ(m_tabRestorer.GetClosedTabs().size(), 2u);
-	EXPECT_EQ(m_tabRestorer.GetClosedTabs().front()->id, tab3.GetId());
-	EXPECT_EQ((*++m_tabRestorer.GetClosedTabs().begin())->id, tab1.GetId());
+	// tab3 was restored, so tab2 and tab4 should remain.
+	const auto &closedTabs = m_tabRestorer.GetClosedTabs();
+	EXPECT_THAT(closedTabs,
+		ElementsAre(Pointee(Field(&PreservedTab::id, tabId4)),
+			Pointee(Field(&PreservedTab::id, tabId2))));
 
 	// The menu should rebuild itself when the set of recent tabs changes, so the menu should now
 	// contain:
 	//
-	// tab3
-	// tab1
+	// tab4
+	// tab2
 	//
-	// and this call should select tab1.
-	m_menuView.SelectItem(m_menuView.GetItemId(1), false, false);
+	// and this call should select tab2.
+	//
+	// Note that middle-clicking an item should have the same effect as left-clicking an item.
+	m_menuView.MiddleClickItem(m_menuView.GetItemId(1), false, false);
+	EXPECT_EQ(tabContainer->GetNumTabs(), 3);
 
-	// tab1 was restored, so only tab3 should remain.
-	ASSERT_EQ(m_tabRestorer.GetClosedTabs().size(), 1u);
-	EXPECT_EQ(m_tabRestorer.GetClosedTabs().front()->id, tab3.GetId());
+	// tab2 was restored, so only tab4 should remain.
+	EXPECT_THAT(closedTabs, ElementsAre(Pointee(Field(&PreservedTab::id, tabId4))));
 
-	// This should restore tab3, leaving no tabs left to be restored.
+	// This should restore tab4, leaving no tabs left to be restored.
 	m_menuView.SelectItem(m_menuView.GetItemId(0), false, false);
+	EXPECT_EQ(tabContainer->GetNumTabs(), 4);
 
-	EXPECT_EQ(m_tabRestorer.GetClosedTabs().size(), 0u);
+	EXPECT_TRUE(closedTabs.empty());
 }
