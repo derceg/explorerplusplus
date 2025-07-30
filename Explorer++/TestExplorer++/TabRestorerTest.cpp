derceg/explorerplusplus

commit 459ae442155f2d43f14c894c4243614ec94e5d19

Update TabContainer::DuplicateTab() to use PreservedTab
This both simplifies the implementation of DuplicateTab(), as well as
allowing the following additional properties to be copied:

- Tab custom name
- Tab lock state
- History entries

Explorer++/TestExplorer++/TabContainerTest.cpp
@@ -313,11 +313,6 @@ TEST_F(TabContainerTest, DuplicateTab)
 
 	m_browser->AddTab(L"e:\\");
 
-	tab2->GetShellBrowser()->SetSortMode(SortMode::Size);
-	tab2->GetShellBrowser()->SetSortDirection(SortDirection::Descending);
-	tab2->GetShellBrowser()->SetViewMode(ViewMode::Details);
-	tab2->GetShellBrowser()->SetAutoArrangeEnabled(false);
-
 	const auto &duplicatedTab = m_tabContainer->DuplicateTab(*tab2);
 
 	// The duplicate tab should be opened to the right of the original tab and be selected.
@@ -325,8 +320,73 @@ TEST_F(TabContainerTest, DuplicateTab)
 	EXPECT_TRUE(m_tabContainer->IsTabSelected(duplicatedTab));
 
 	EXPECT_EQ(duplicatedTab.GetShellBrowser()->GetDirectory(), pidl);
+}
+
+TEST_F(TabContainerTest, DuplicateTabWithCustomName)
+{
+	auto *tab = m_browser->AddTab(L"c:\\");
+
+	std::wstring customName = L"Custom name";
+	tab->SetCustomName(customName);
+
+	const auto &duplicatedTab = m_tabContainer->DuplicateTab(*tab);
+	EXPECT_EQ(duplicatedTab.GetName(), customName);
+}
+
+TEST_F(TabContainerTest, DuplicateTabWithLock)
+{
+	auto *tab = m_browser->AddTab(L"c:\\");
+
+	tab->SetLockState(Tab::LockState::Locked);
+
+	const auto &duplicatedTab = m_tabContainer->DuplicateTab(*tab);
+	EXPECT_EQ(duplicatedTab.GetLockState(), Tab::LockState::Locked);
+}
+
+TEST_F(TabContainerTest, DuplicateTabWithAddressLock)
+{
+	auto *tab = m_browser->AddTab(L"c:\\");
+
+	tab->SetLockState(Tab::LockState::AddressLocked);
+
+	const auto &duplicatedTab = m_tabContainer->DuplicateTab(*tab);
+	EXPECT_EQ(duplicatedTab.GetLockState(), Tab::LockState::AddressLocked);
+}
+
+TEST_F(TabContainerTest, DuplicateTabWithFolderSettings)
+{
+	const auto *tab = m_browser->AddTab(L"c:\\");
+
+	tab->GetShellBrowser()->SetSortMode(SortMode::Size);
+	tab->GetShellBrowser()->SetSortDirection(SortDirection::Descending);
+	tab->GetShellBrowser()->SetViewMode(ViewMode::Details);
+	tab->GetShellBrowser()->SetAutoArrangeEnabled(false);
+
+	const auto &duplicatedTab = m_tabContainer->DuplicateTab(*tab);
 	EXPECT_EQ(duplicatedTab.GetShellBrowser()->GetFolderSettings(),
-		tab2->GetShellBrowser()->GetFolderSettings());
+		tab->GetShellBrowser()->GetFolderSettings());
+}
+
+TEST_F(TabContainerTest, DuplicateTabWithHistory)
+{
+	PidlAbsolute pidl;
+	auto *tab = m_browser->AddTab(L"c:\\", {}, &pidl);
+	NavigateTab(tab, L"d:\\");
+	NavigateTab(tab, L"e:\\");
+	tab->GetShellBrowser()->GetNavigationController()->GoBack();
+
+	const auto &duplicatedTab = m_tabContainer->DuplicateTab(*tab);
+
+	// The history of the original tab should be duplicated.
+	const auto *navigationController = duplicatedTab.GetShellBrowser()->GetNavigationController();
+	ASSERT_EQ(navigationController->GetNumHistoryEntries(), 3);
+	EXPECT_EQ(navigationController->GetCurrentIndex(), 1);
+	EXPECT_EQ(navigationController->GetEntryAtIndex(0)->GetPidl(),
+		CreateSimplePidlForTest(L"c:\\"));
+	EXPECT_EQ(navigationController->GetEntryAtIndex(1)->GetPidl(),
+		CreateSimplePidlForTest(L"d:\\"));
+	EXPECT_EQ(navigationController->GetEntryAtIndex(2)->GetPidl(),
+		CreateSimplePidlForTest(L"e:\\"));
 }
 
 TEST_F(TabContainerTest, CloseTab)

Explorer++/Explorer++/TabContainer.cpp
@@ -697,13 +697,8 @@ std::vector<Tab *> TabContainer::GetAllTabsInOrder() const
 
 Tab &TabContainer::DuplicateTab(const Tab &tab)
 {
-	auto *shellBrowser = tab.GetShellBrowser();
-	const auto &folderSettings = shellBrowser->GetFolderSettings();
-	const auto &folderColumns = shellBrowser->GetColumns();
-	auto navigateParams = NavigateParams::Normal(shellBrowser->GetDirectory().Raw());
-	return CreateNewTab(navigateParams,
-		TabSettings(_index = GetTabIndex(tab) + 1, _selected = true), &folderSettings,
-		&folderColumns);
+	PreservedTab preservedTab(tab, GetTabIndex(tab) + 1);
+	return CreateNewTab(preservedTab);
 }
 
 void TabContainer::OnTabMoved(int fromIndex, int toIndex)

commit 3a87589aff4d245dc0d52e13777a28df75c3740a

Save and restore columns when creating tab from preserved tab
This means that when a tab is closed and then reopened, the tab's
columns will be restored.

Additionally, when a tab is duplicated, the columns will also be
restored. This behaviour was originally present, but was broken by
459ae442155f2d43f14c894c4243614ec94e5d19, as reported in
https://explorerplusplus.com/forum/viewtopic.php?t=3519.

Explorer++/Explorer++/ShellBrowser/PreservedFolderState.cpp
@@ -7,6 +7,7 @@
 #include "ShellBrowser.h"
 
 PreservedFolderState::PreservedFolderState(const ShellBrowser *shellBrowser) :
-	folderSettings(shellBrowser->GetFolderSettings())
+	folderSettings(shellBrowser->GetFolderSettings()),
+	folderColumns(shellBrowser->GetAllColumnSets())
 {
 }

Explorer++/Explorer++/ShellBrowser/PreservedFolderState.h
@@ -16,4 +16,5 @@ public:
 	PreservedFolderState(const ShellBrowser *shellBrowser);
 
 	FolderSettings folderSettings;
+	FolderColumns folderColumns;
 };

Explorer++/Explorer++/ShellBrowser/ShellBrowserImpl.cpp
@@ -45,7 +45,7 @@ ShellBrowserImpl::ShellBrowserImpl(HWND owner, App *app, BrowserWindow *browser,
 	const std::vector<std::unique_ptr<PreservedHistoryEntry>> &history, int currentEntry,
 	const PreservedFolderState &preservedFolderState) :
 	ShellBrowserImpl(owner, app, browser, fileActionHandler, preservedFolderState.folderSettings,
-		nullptr)
+		&preservedFolderState.folderColumns)
 {
 	m_navigationController = std::make_unique<ShellNavigationController>(this, browser,
 		&m_navigationManager, m_app->GetNavigationEvents(), history, currentEntry);

Explorer++/TestExplorer++/ShellBrowserFake.cpp
@@ -13,7 +13,8 @@
 ShellBrowserFake::ShellBrowserFake(BrowserWindow *browser, NavigationEvents *navigationEvents,
 	const std::vector<std::unique_ptr<PreservedHistoryEntry>> &preservedEntries, int currentEntry,
 	const PreservedFolderState &preservedFolderState) :
-	ShellBrowserFake(browser, navigationEvents, preservedFolderState.folderSettings)
+	ShellBrowserFake(browser, navigationEvents, preservedFolderState.folderSettings,
+		preservedFolderState.folderColumns)
 {
 	m_navigationController = std::make_unique<ShellNavigationController>(this, browser,
 		&m_navigationManager, navigationEvents, preservedEntries, currentEntry);

Explorer++/TestExplorer++/TabContainerTest.cpp
@@ -367,6 +367,18 @@ TEST_F(TabContainerTest, DuplicateTabWithFolderSettings)
 		tab->GetShellBrowser()->GetFolderSettings());
 }
 
+TEST_F(TabContainerTest, DuplicateTabWithColumns)
+{
+	const auto *tab = m_browser->AddTab(L"c:\\");
+
+	FolderColumns folderColumns;
+	folderColumns.realFolderColumns = { { ColumnType::Name, TRUE, 100 } };
+	tab->GetShellBrowser()->SetAllColumnSets(folderColumns);
+
+	const auto &duplicatedTab = m_tabContainer->DuplicateTab(*tab);
+	EXPECT_EQ(duplicatedTab.GetShellBrowser()->GetAllColumnSets(), folderColumns);
+}
+
 TEST_F(TabContainerTest, DuplicateTabWithHistory)
 {
 	PidlAbsolute pidl;

Explorer++/TestExplorer++/TabRestorerTest.cpp
@@ -187,6 +187,26 @@ TEST_F(TabRestorerTest, RestoreTabWithCustomName)
 	EXPECT_EQ(restoredTab->GetName(), customName);
 }
 
+TEST_F(TabRestorerTest, RestoreTabWithColumns)
+{
+	auto *browser = AddBrowser();
+	browser->AddTab(L"c:\\");
+	int tabId2 = browser->AddTabAndReturnId(L"d:\\");
+
+	auto *tabContainer = browser->GetActiveTabContainer();
+
+	FolderColumns folderColumns;
+	folderColumns.realFolderColumns = { { ColumnType::Name, TRUE, 100 } };
+	tabContainer->GetTab(tabId2).GetShellBrowser()->SetAllColumnSets(folderColumns);
+
+	EXPECT_TRUE(tabContainer->CloseTab(tabContainer->GetTab(tabId2)));
+
+	auto *restoredTab = m_tabRestorer.RestoreTabById(tabId2);
+	ASSERT_NE(restoredTab, nullptr);
+
+	EXPECT_EQ(restoredTab->GetShellBrowser()->GetAllColumnSets(), folderColumns);
+}
+
 TEST_F(TabRestorerTest, StateDuringRestore)
 {
 	auto *browser = AddBrowser();